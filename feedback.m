4ea72ba90dc43e1ea7e31265169c2b030547e363
Tom Kelley <th.kelley@neu.edu>
2020-01-20 17:43:19 -0500

    Create feedback.m

---
+++ feedback.m
@@ -0,0 +1,2 @@
+## Jan 30
+Clear and concise code with sufficient comments - looks great. One thing that needs changing now is how you output the results when `T` is an array - something to thing about.
