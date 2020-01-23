25bacb68b0ca3a422a65cda225b7a78378b827f9
LockeT-NEU <59491399+LockeT-NEU@users.noreply.github.com>
2020-01-23 16:19:38 -0500

    1

--- Problem4a.m
+++ Problem4a.m
@@ -1,4 +1,19 @@
-csvperiodic = 'smallperiodictable.csv'
-csvdata = readtable(csvperiodic, 'HeaderLines', 0)
-txtperiodic = 'smallperiodictable.txt'
-txtdata = readtable(txtperiodic, 'HeaderLines', 0)
+csvperiodic = 'C:\Users\tyler\Desktop\School\PHYS1211\problem4\smallperiodictable.csv'
+datatable = readtable(csvperiodic)
+datastruct = table2struct(datatable)
+
+%question 1
+densenames = sort(datatable.Element(datatable.Density > datatable.Density(80) & datatable.Isotopes < 30))
+densenum = numel(names)
+%Question 2
+symbols = datatable.Symbol(datatable.DiscoveryYear>=1900 & datatable.DiscoveryYear<2000)
+avgweight = mean(datatable.AtomicWeight(datatable.DiscoveryYear>=1900 & datatable.DiscoveryYear<2000))
+
+%question 3 
+sorted = sortrows(datatable, 6, 'descend')
+topten = sorted.Element(1:10)
+
+%Question 4: Tables are generally easier to work with than structs, but
+%they also require you to have the same number of rows for every variable,
+%so it is not useful for cases where you aare working with different sized
+%lists
