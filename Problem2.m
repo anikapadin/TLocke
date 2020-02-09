d82d6c28ba9928d2d45475a231991758d22f0ffe
LockeT-NEU <59491399+LockeT-NEU@users.noreply.github.com>
2020-02-09 18:03:35 -0500

    Update Problem2.m

--- Problem2.m
+++ Problem2.m
@@ -3,17 +3,42 @@ G = 6.67408e-11;
 M = 5.972e24;
 R = 6.371e6;
 %get T here(minutes)
-T = input("Period in minutes: ");
-
+T = strsplit(input("Period followed by unit(s/m/h/d) separated by commas: ", 's'), ',');
+T = strtrim(T);
+[nums, tf] = str2num(T)
+nums = nums(tf)
+units = T(~tf)
+while numel(nums) ~= numel(units)
+    error("Please enter only numeric values")
+    T = input("Period in minutes: ", 'x');
+    T = strtrim(T)
+    [nums, tf] = str2num(T)
+    nums = nums(tf)
+    units = T(~tf)
+end
 %Convert T to seconds
-T =  T * 60;
+switch t
+    case 's'
+    case 'm'
+        T =  T * 60;
+    case 'h'
+        T = T * 3600
+    case 'd'
+        T = T * 3600 * 24
+    otherwise
+        error("Invalid Units")
+end
 
 %Calculate height and velocity
 h = ((G*M*T.^2)/(4*pi^2)).^(1/3) - R;
 v = (2 * pi * (R + h))/T;
 %convert h to kilometers and print
-h = h/1000;
-disp(round(h, 3, "significant") + " km, " + round(v, 3, "significant") + " m/s")
+if h < 0
+    error("Orbit is impossible")
+else
+    h = h/1000;
+    disp(round(h, 3, "significant") + " km, " + round(v, 3, "significant") + " m/s")
+end
 
 %answer questions here
 %Q1: 45 min: "-2180 km" impossible. 90 min: 281 km.  1 day: 35800 km
